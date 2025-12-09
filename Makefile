13b490f2cd6c6d3d90f16ca30afcddd596c5420c
Marcin Lusa <mlooser@gmail.com>
2025-12-09 11:54:50 +0100

    fix compilation

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # Compiler and flags
 CXX = g++
-CXXFLAGS = -std=c++17 -Wall -Wextra
-LDFLAGS = `pkg-config --cflags --libs sdl2`
+CXXFLAGS = -std=c++17 -Wall -Wextra -I"./libs/" -DSOL_ALL_SAFETIES_ON=1 `pkg-config --cflags lua`
+LDFLAGS = `pkg-config --cflags --libs sdl2 lua` -lSDL2_image -lSDL2_mixer -lSDL2_ttf
 
 # Add more libraries as you progress:
 # LDFLAGS = `pkg-config --cflags --libs sdl2 SDL2_image SDL2_mixer lua`
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
+
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    glm::vec2 velocity(0.0f, 0.0f);
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    std::cout << "Deps are fine" << std::endl;
     return 0;
 }
